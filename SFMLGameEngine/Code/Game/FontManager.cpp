78c72bd5d220dad9419bda4273c4329f7a92d7f7
Mo Agilah <32136453+MoAgilah@users.noreply.github.com>
2025-01-05 14:53:06 +0000

    Commit some sprites and fonts for the game. Extend the functionality of the font manager in the engine. Add some initial designs for game object elements.

--- SFMLGameEngine/Code/Game/FontManager.cpp
+++ SFMLGameEngine/Code/Game/FontManager.cpp
@@ -4,3 +4,10 @@ FontManager::FontManager()
 {
 	m_standardFont.loadFromFile("../SFMLGameEngine/Resources/Fonts/arial.ttf");
 }
+
+void FontManager::AddFont(std::string path)
+{
+	sf::Font font;
+	font.loadFromFile(path);
+	m_fonts.push_back(font);
+}
--- SFMLGameEngine/Code/Game/FontManager.h
+++ SFMLGameEngine/Code/Game/FontManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <SFML/Graphics.hpp>
 
 class FontManager
@@ -8,10 +9,14 @@ public:
 	FontManager();
 	~FontManager() = default;
 
+	void AddFont(std::string path);
+
+
 	const sf::Font& GetStandardFont() { return m_standardFont; }
 	const sf::Font& GetMenuFont() { return m_standardFont; }
 
 private:
 
 	sf::Font m_standardFont;
+	std::vector<sf::Font> m_fonts;
 };
